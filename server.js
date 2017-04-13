308f26ef8d8578c2756870c8dd96be955fe33465
Jogomez13 <jonathan.gomez.web@gmail.com>
2017-04-13 14:46:25 +0200

    mise à jour du port

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ var express = require ('express');
 
 var serveur = express();
 
-server.get("/",function(request,response){
+serveur.get("/",function(request,response){
   response.send("message");
 });
 
-server.get("/toto",function(request,response){
+serveur.get("/toto",function(request,response){
   response.sendFile(__dirname + "view/index.html");
 });
 
 
-server.listen(process.env.PORT || 12107);
+serveur.listen(process.env.PORT || 12107);
